691f59c7d15c4248b137aa7b0fc96c132947dfb3
lunarnublet <lunarnublet@gmail.com>
2017-07-26 08:49:35 -0600

    basic structure.

---
+++ burger-website/index.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var pug = require("pug");
+var path = require("path");
+
+var app = express();
+
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use(express.static(path.join(__dirname + "/public")));
+
+
+app.get("/", function(req, res)
+{
+    res.render("index");
+});
+
+
+app.listen(3000);
