30b8d28a1e496d8f7482e28a0212f9bc5e7accf5
lunarnublet <lunarnublet@gmail.com>
2017-07-28 08:49:06 -0600

    made daily combo more clear.

--- burger-website/public/js/daily-combo.js
+++ burger-website/public/js/daily-combo.js
@@ -2,9 +2,17 @@
 var combolist = document.getElementById("dailyCombo");
 function SetComboList()
 {
-    var li = document.createElement("li");
-    li.appendChild(document.createTextNode(Math.floor(Math.random() * 3)));
-    combolist.appendChild(li);
+    var li1 = document.createElement("li");
+    li1.appendChild(document.createTextNode("Burger: " + Math.floor(Math.random() * 3)));
+    combolist.appendChild(li1);
+
+    var li2 = document.createElement("li");
+    li2.appendChild(document.createTextNode("Side: " + Math.floor(Math.random() * 3)));
+    combolist.appendChild(li2);
+
+    var li3 = document.createElement("li");
+    li3.appendChild(document.createTextNode("Drink: " + Math.floor(Math.random() * 3)));
+    combolist.appendChild(li3);
 }
 
 SetComboList();
