a050ce2801dad4e77405ce2ad2a8efe6ab7860b2
jouzune <dmatson@student.neumont.edu>
2017-08-03 08:57:03 -0600

    added 404 page, /menu route is safe

--- burger-website/index.js
+++ burger-website/index.js
@@ -9,27 +9,41 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use(express.static(path.join(__dirname, "/public")));
 
-app.get("/", function(req, res)
-{
+app.get("/", function (req, res) {
     var entreeNumber = Math.floor(Math.random() * config.entrees.length);
     var drinkNumber = Math.floor(Math.random() * config.drinks.length);
     var sideNumber = Math.floor(Math.random() * config.sides.length);
-    res.render("index",{config: config, entreeNumber: entreeNumber, drinkNumber: drinkNumber, sideNumber: sideNumber});
+    res.render("index", { config: config, entreeNumber: entreeNumber, drinkNumber: drinkNumber, sideNumber: sideNumber });
 });
-
 app.get("/directions", function (req, res) {
-    res.render("directions", {config: config});
+    res.render("directions", { config: config });
+});
+app.get("/menu/:page", function (req, res, next) {
+    if (!isValidMenuPage(req.params.page)) {
+        next();
+    }
+
+    res.render(req.params.page, {
+        title: capitalizeFirstLetter(req.params.page),
+        config: config,
+        menu_items: config[req.params.page]
+    });
 });
 
-app.get("/menu/:page", function (req, res) {
-    res.render(req.params.page,
-        {title: capitalizeFirstLetter(req.params.page),
-            config: config,
-        menu_items: config[req.params.page]});
+app.use(function (req, res, next) {
+    res.status(404).send('404, not found');
 });
+
+app.listen(3000);
 
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
+function isValidMenuPage(page) {
+    var pages = ["entrees", "sides", "drinks"];
 
-app.listen(3000);
+    for(let i = 0; i < pages.length; ++i) {
+        if (page == pages[i]) return true;
+    }
+    return false;
+}
