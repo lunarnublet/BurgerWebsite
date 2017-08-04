3bf1999c90efbfd54e2eca194575b79d90f3bfdb
Dan Christensen <816.dan@gmail.com>
2017-08-04 14:39:09 +0000

    Added a pin drop to the directions page map.

--- burger-website/public/js/directions.js
+++ burger-website/public/js/directions.js
@@ -1,12 +1,20 @@
 var burgers = {}
 
 burgers.initMap = function() {
+    var myLatLng = {lat: 40.766246, lng: -111.8908555};
+
     burgers.map = new google.maps.Map(document.getElementById('map'), {
         zoom: 18,
-        center: {lat: 40.766246, lng: -111.8908555},
+        center: myLatLng
+    });
+
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: burgers.map,
+        title: 'Express Burger'
     });
 
     // if (navigator.geolocation) {
 
-    // }
+        // }
 }
